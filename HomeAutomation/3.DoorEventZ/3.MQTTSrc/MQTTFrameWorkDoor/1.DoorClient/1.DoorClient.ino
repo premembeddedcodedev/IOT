a97f3de8d1752949f0a952c1be90615b8902db6b
praveenv <prem441@gmail.com>
2022-08-23 12:23:51 +0530

    working with timestamp

--- HomeAutomation/3.DoorEventZ/3.MQTTSrc/MQTTFrameWorkDoor/1.DoorClient/1.DoorClient.ino
+++ HomeAutomation/3.DoorEventZ/3.MQTTSrc/MQTTFrameWorkDoor/1.DoorClient/1.DoorClient.ino
@@ -4,12 +4,28 @@
 #include <ArduinoOTA.h>
 #include <PubSubClient.h>
 #include <ArduinoJson.h>
+#include <NTPClient.h>
+#include <ESP8266WiFiMulti.h>
+ESP8266WiFiMulti wifiMulti;
+
+const long utcOffsetInSeconds = 3600;
+
+char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+
+WiFiUDP ntpUDP;
+//NTPClient timeClient(ntpUDP, "time.nist.gov", utcOffsetInSeconds);
+NTPClient timeClient(ntpUDP, "in.pool.ntp.org", utcOffsetInSeconds);
+
+const uint32_t connectTimeoutMs = 5000;
+int hours;
+int minutes;
+int seconds;
 
 // Update these with values suitable for your network.
 
-const char* ssid = "TP-Link_F524";
-const char* password = "XXXXXXXXXXXX";
 const char* mqtt_server = "192.168.29.65";
+
+char destination[17];
 
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -18,30 +34,6 @@ unsigned long lastMsg = 0;
 #define MSG_BUFFER_SIZE  (50)
 char msg[MSG_BUFFER_SIZE];
 int value = 0;
-
-void setup_wifi() {
-
-	delay(10);
-	// We start by connecting to a WiFi network
-	Serial.println();
-	Serial.print("Connecting to ");
-	Serial.println(ssid);
-
-	WiFi.mode(WIFI_STA);
-	WiFi.begin(ssid, password);
-
-	while (WiFi.status() != WL_CONNECTED) {
-		delay(500);
-		Serial.print(".");
-	}
-
-	randomSeed(micros());
-
-	Serial.println("");
-	Serial.println("WiFi connected");
-	Serial.println("IP address: ");
-	Serial.println(WiFi.localIP());
-}
 
 void callback(char* topic, byte* payload, unsigned int length) {
 	Serial.print("Message arrived [");
@@ -115,25 +107,137 @@ void ota_config()
 	ArduinoOTA.begin();
 }
 
+void scan()
+{
+#if 1
+	String ssid;
+	int32_t rssi;
+	uint8_t encryptionType;
+	uint8_t* bssid;
+	int32_t channel;
+	bool hidden;
+	int scanResult;
+
+	Serial.println(F("Starting WiFi scan..."));
+
+	scanResult = WiFi.scanNetworks(/*async=*/false, /*hidden=*/true);
+
+	if (scanResult == 0) {
+		Serial.println(F("No networks found"));
+	} else if (scanResult > 0) {
+		Serial.printf(PSTR("%d networks found:\n"), scanResult);
+
+		// Print unsorted scan results
+		for (int8_t i = 0; i < scanResult; i++) {
+			WiFi.getNetworkInfo(i, ssid, encryptionType, rssi, bssid, channel, hidden);
+
+			Serial.printf(PSTR("  %02d: [CH %02d] [%02X:%02X:%02X:%02X:%02X:%02X] %ddBm %c %c %s\n"),
+					i,
+					channel,
+					bssid[0], bssid[1], bssid[2],
+					bssid[3], bssid[4], bssid[5],
+					rssi,
+					(encryptionType == ENC_TYPE_NONE) ? ' ' : '*',
+					hidden ? 'H' : 'V',
+					ssid.c_str());
+			delay(0);
+		}
+	} else {
+		Serial.printf(PSTR("WiFi scan error %d"), scanResult);
+	}
+#else
+	// WiFi.scanNetworks will return the number of networks found
+	int n = WiFi.scanNetworks();
+	Serial.println("scan done");
+	if (n == 0) {
+		Serial.println("no networks found");
+	}
+	else {
+		Serial.print(n);
+		Serial.println(" networks found");
+		for (int i = 0; i < n; ++i) {
+			Serial.print(i + 1);
+			Serial.print(": ");
+			Serial.print(WiFi.SSID(i));
+			Serial.print(" (");
+			Serial.print(WiFi.RSSI(i));
+			Serial.print(")");
+			Serial.println();
+			delay(10);
+		}
+	}
+#endif
+}
+
+void wifi_scan_config()
+{
+	WiFi.disconnect();
+	scan();
+	WiFi.persistent(false);
+	wifiMulti.addAP("SHSIAAP2", "prem@123");
+	wifiMulti.addAP("JioFiber5G", "prem@123");
+	wifiMulti.addAP("JioFiber4g", "prem@123");
+	wifiMulti.addAP("TP-Link_F524", "prem@123");
+	wifiMulti.addAP("TP-Link_F524_5G", "prem@123");
+
+	if (wifiMulti.run(connectTimeoutMs) == WL_CONNECTED) {
+		Serial.print("WiFi connected: ");
+		Serial.print(WiFi.SSID());
+		Serial.print(" ");
+		Serial.println(WiFi.localIP());
+	} else {
+		Serial.println("WiFi not connected!");
+		ESP.restart();
+	}
+}
 
 void setup() {
 	pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
 	pinMode(14, INPUT_PULLUP);
 	Serial.begin(9600);
 	Serial.println("Publisher: Door Events Node -1");
-	setup_wifi();
+	wifi_scan_config();
 	ota_config();
 	client.setServer(mqtt_server, 1883);
 	client.setCallback(callback);
+	timeClient.begin();
 }
 
 void loop()
 {
-	ArduinoOTA.handle();
 	if (!client.connected()) {
 		reconnect();
 	}
-	client.loop();
+	ArduinoOTA.handle();
+	timeClient.update();
+	Serial.println(timeClient.getFormattedTime());
+	strcpy(destination, daysOfTheWeek[timeClient.getDay()]);
+	Serial.println(destination);
+
+	hours = timeClient.getHours();
+	if(hours <= 19) {
+		hours = timeClient.getHours() + 4;
+	} else {
+		hours = timeClient.getHours() - 20; //till 12 we are taking care in if
+	}
+
+	minutes = timeClient.getMinutes();
+	if(minutes <= 29) {
+		minutes = timeClient.getMinutes() + 30;
+	} else {
+		minutes = timeClient.getMinutes() - 30;
+		hours += 1;
+	}
+	seconds = timeClient.getSeconds();
+	
+	Serial.print(", ");
+	Serial.print(hours);
+	Serial.print(":");
+	Serial.print(minutes);
+	Serial.print(":");
+	Serial.println(seconds);
+
+
 	if (digitalRead(14) == 1){
 		char out[128];
 		StaticJsonDocument<256> doc;
@@ -144,5 +248,10 @@ void loop()
 		boolean rc = client.publish("DoorEvents", out); 
 	}
 
-	delay(2000);
+	if ( WiFi.status() != WL_CONNECTED ) {
+		ESP.restart();
+	}
+	client.loop();
+
+	delay(5000);
 }
