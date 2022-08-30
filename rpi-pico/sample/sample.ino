f2f4ce4035fad09c17b37434fab70805dd243719
praveenv <prem441@gmail.com>
2022-08-30 11:50:33 +0530

    pico sample code and flashing procedure

---
+++ rpi-pico/sample/sample.ino
@@ -0,0 +1,11 @@
+
+void setup() {
+	Serial.begin(9600);
+	delay(8000);
+	Serial.println("Hello world -- 30-08-2022");
+}
+
+void loop() {
+	Serial.println("897 - loop praveen - just check");
+	delay(5000);
+}
